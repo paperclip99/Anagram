33a7acf03cbd951bc491b30ea3b08483b2a97a79
Azimjon Fayzulloev <56757825+corecudr@users.noreply.github.com>
2021-04-07 07:28:03 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+anagramm = {}
+
+class Anagram(Resource):
+    def get(self):
+        word = request.args.get("word")
+        answer = []
+        for i in anagramm["load"]:
+            b = ' '.join(i).split()
+            b.sort()
+            c = ' '.join(word).split()
+            c.sort()
+            if c == b: answer.append(i)
+        if len(answer) == 0: return None
+        else: return answer
+
+    def put(self):
+        anagramm["load"] = request.get_json()
+        return anagramm["load"]
+
+api.add_resource(Anagram, '/load', "/get")
+
+if __name__ == '__main__':
+    app.run(debug=True)
